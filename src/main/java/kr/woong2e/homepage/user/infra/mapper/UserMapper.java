0c61b706a8aeb9c2df965d814f0d47c0e97e858a
woong2e <tlswodnd12345@naver.com>
2025-04-01 23:58:49 +0900

    [feat]: user entity를 도메인 객체로 변환하는 메서드를 구현한다.

---
+++ src/main/java/kr/woong2e/homepage/user/infra/mapper/UserMapper.java
@@ -0,0 +1,27 @@
+package kr.woong2e.homepage.user.infra.mapper;
+
+import kr.woong2e.homepage.user.domain.domain.User;
+import kr.woong2e.homepage.user.infra.entity.UserEntity;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserMapper {
+
+//    public UserEntity toEntity(User users){
+//        return UserEntity.of(
+//                users.getEmail(),
+//                users.getPassword()
+//        );
+//    }
+
+    public User toDomain(UserEntity userEntity){
+        return User.of(
+                userEntity.getId(),
+                userEntity.getNickname(),
+                userEntity.getEmail(),
+                userEntity.getProfileImage(),
+                userEntity.getRole(),
+                userEntity.getCreateDate()
+        );
+    }
+}
