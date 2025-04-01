6d3ca422cd382f1127d90aab7920b46575d9a2eb
woong2e <tlswodnd12345@naver.com>
2025-04-01 23:57:46 +0900

    [feat]: user 도메인에 대한 repository를 설정한다.

---
+++ src/main/java/kr/woong2e/homepage/user/infra/repository/UserJpaRepository.java
@@ -0,0 +1,8 @@
+package kr.woong2e.homepage.user.infra.repository;
+
+import kr.woong2e.homepage.user.infra.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserJpaRepository extends JpaRepository<UserEntity, Long> {
+
+}
---
+++ src/main/java/kr/woong2e/homepage/user/infra/repository/UserRepositoryImpl.java
@@ -0,0 +1,24 @@
+package kr.woong2e.homepage.user.infra.repository;
+
+import kr.woong2e.homepage.user.domain.domain.User;
+import kr.woong2e.homepage.user.domain.repository.UserRepository;
+import kr.woong2e.homepage.user.infra.mapper.UserMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+@RequiredArgsConstructor
+public class UserRepositoryImpl implements UserRepository {
+
+    private final UserJpaRepository userJpaRepository;
+    private final UserMapper userMapper;
+
+    @Override
+    public Optional<User> findById(Long userId) {
+        return userJpaRepository.findById(userId)
+                .map(userMapper::toDomain);
+    }
+
+}
