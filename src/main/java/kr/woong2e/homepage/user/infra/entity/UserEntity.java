cb94ddf9e29c279ee2d2fc695a21e78416c4156e
woong2e <tlswodnd12345@naver.com>
2025-04-01 23:56:11 +0900

    [feat]: user에 대한 entity를 정의한다.

---
+++ src/main/java/kr/woong2e/homepage/user/infra/entity/UserEntity.java
@@ -0,0 +1,57 @@
+package kr.woong2e.homepage.user.infra.entity;
+
+import jakarta.persistence.*;
+import kr.woong2e.homepage.user.domain.value.Role;
+import kr.woong2e.homepage.user.domain.value.SocialProvider;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "users")
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "login_id", length = 20, unique = true)
+    private String loginId;
+
+    @Column(name = "password", length = 30, nullable = false)
+    private String password;
+
+    @Column(name = "nickname", nullable = false)
+    private String nickname;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "profile_image")
+    private String profileImage;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role", nullable = false)
+    private Role role;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "provider")
+    private SocialProvider provider;
+
+    @Column(name = "provider_id")
+    private String providerId;
+
+    @Column(name = "create_date", nullable = false, updatable = false)
+    private LocalDateTime createDate;
+
+    @Column(name = "modify_date")
+    private LocalDateTime modifyDate;
+
+//    public static UserEntity of(String nickname, String email, String profileImage, Role role, LocalDateTime createDate) {
+//        return new UserEntity(null, null, nickname, email, profileImage, role, null, null, createDate, null);
+//    }
+
+}
