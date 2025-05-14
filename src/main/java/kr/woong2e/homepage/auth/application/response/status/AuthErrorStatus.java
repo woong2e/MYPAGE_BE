0b63225b786aab7c4dfbd3b5d718f191525c126a
woong2e <tlswodnd12345@naver.com>
2025-05-14 17:30:31 +0900

    [feat]: 인증과 관련된 에러코드를 추가한다. (#6)

--- src/main/java/kr/woong2e/homepage/auth/application/response/status/AuthErrorStatus.java
+++ src/main/java/kr/woong2e/homepage/auth/application/response/status/AuthErrorStatus.java
@@ -10,7 +10,11 @@ import org.springframework.http.HttpStatus;
 public enum AuthErrorStatus implements BaseStatus {
 
     DUPLICATED_ID(HttpStatus.CONFLICT, "DI", "Duplicated id."),
-    MAIL_DELIVERY_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "ESE", "Failed to Send Email.");
+    MAIL_DELIVERY_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "ESE", "Failed to Send Email."),
+    CERTIFICATION_NOT_EXIST(HttpStatus.NOT_FOUND, "CNE", "존재하지 않는 이메일 인증입니다."),
+    INVALID_EMAIL(HttpStatus.NOT_FOUND, "IE", "존재하지 않은 이메일"),
+    INVALID_CERTIFICATION_NUMBER(HttpStatus.BAD_REQUEST, "ICN", "유효하지 않은 인증번호"),
+    CERTIFICATION_FAILED(HttpStatus.UNAUTHORIZED, "CF", "메일 인증 실패");
 
     private final HttpStatus httpStatus;
     private final String code;
