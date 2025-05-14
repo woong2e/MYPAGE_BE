89a5b16dbfcf7f0689eb5b50700e28584c622e25
woong2e <tlswodnd12345@naver.com>
2025-05-14 17:56:31 +0900

    [feat]: 회원가입 로직을 구현한다. (#6)
    이메일 인증, 인증확인, 회원가입

--- src/main/java/kr/woong2e/homepage/auth/application/service/AuthService.java
+++ src/main/java/kr/woong2e/homepage/auth/application/service/AuthService.java
@@ -1,8 +1,14 @@
 package kr.woong2e.homepage.auth.application.service;
 
+import kr.woong2e.homepage.auth.api.request.CheckCertificationRequestDto;
+import kr.woong2e.homepage.auth.api.request.EmailCertificationRequestDto;
 import kr.woong2e.homepage.auth.api.request.IdCheckRequestDto;
+import kr.woong2e.homepage.auth.api.request.SignUpRequestDto;
 
 public interface AuthService {
 
     void idCheck(IdCheckRequestDto idCheckRequestDto);
+    void emailCertification(EmailCertificationRequestDto emailCertificationRequestDto);
+    void checkCertification(CheckCertificationRequestDto checkCertificationRequestDto);
+    void signUp(SignUpRequestDto signUpRequestDto);
 }
--- src/main/java/kr/woong2e/homepage/auth/application/service/implement/AuthServiceImpl.java
+++ src/main/java/kr/woong2e/homepage/auth/application/service/implement/AuthServiceImpl.java
@@ -1,11 +1,19 @@
 package kr.woong2e.homepage.auth.application.service.implement;
 
+import kr.woong2e.homepage.auth.api.request.CheckCertificationRequestDto;
+import kr.woong2e.homepage.auth.api.request.EmailCertificationRequestDto;
 import kr.woong2e.homepage.auth.api.request.IdCheckRequestDto;
+import kr.woong2e.homepage.auth.api.request.SignUpRequestDto;
 import kr.woong2e.homepage.auth.application.response.status.AuthErrorStatus;
 import kr.woong2e.homepage.auth.application.service.AuthService;
+import kr.woong2e.homepage.auth.domain.Certification;
+import kr.woong2e.homepage.auth.domain.CertificationRepository;
 import kr.woong2e.homepage.global.exception.CustomException;
-import kr.woong2e.homepage.user.domain.repository.UserRepository;
+import kr.woong2e.homepage.global.provider.EmailProvider;
+import kr.woong2e.homepage.user.domain.User;
+import kr.woong2e.homepage.user.domain.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,10 +21,12 @@ import org.springframework.stereotype.Service;
 public class AuthServiceImpl implements AuthService {
 
     private final UserRepository userRepository;
+    private final CertificationRepository certificationRepository;
+    private final EmailProvider emailProvider;
+    private final PasswordEncoder encoder;
     
     @Override
     public void idCheck(IdCheckRequestDto idCheckRequestDto) {
-
         String loginId = idCheckRequestDto.getLoginId();
         boolean isExistsId = userRepository.existsByLoginId(loginId);
 
@@ -24,5 +34,58 @@ public class AuthServiceImpl implements AuthService {
             throw new CustomException(AuthErrorStatus.DUPLICATED_ID);
         }
 
+    }
+
+    @Override
+    public void emailCertification(EmailCertificationRequestDto emailCertificationRequestDto) {
+        String loginId = emailCertificationRequestDto.getLoginId();
+        String email = emailCertificationRequestDto.getEmail();
+
+        boolean isExistsId = userRepository.existsByLoginId(loginId);
+        if (isExistsId) {
+            throw new CustomException(AuthErrorStatus.DUPLICATED_ID);
+        }
+
+        Certification certification = Certification.create(loginId, email);
+        String certificationNumber = certification.getCertificationNumber();
+        emailProvider.sendCertificationMail(email, certificationNumber);
+
+        certificationRepository.save(certification);
+    }
+
+    @Override
+    public void checkCertification(CheckCertificationRequestDto checkCertificationRequestDto) {
+        String loginId = checkCertificationRequestDto.getLoginId();
+        String email = checkCertificationRequestDto.getEmail();
+        String certificationNumber = checkCertificationRequestDto.getCertificationNumber();
+
+        Certification certification = certificationRepository.findByLoginId(loginId)
+                .orElseThrow(() -> new CustomException(AuthErrorStatus.CERTIFICATION_NOT_EXIST));
+
+        certification.verify(email, certificationNumber);
+        certificationRepository.save(certification);
+    }
+
+    @Override
+    public void signUp(SignUpRequestDto signUpRequestDto) {
+        String loginId = signUpRequestDto.loginId();
+        boolean isExistId = userRepository.existsByLoginId(loginId);
+        if (isExistId) {
+            throw new CustomException(AuthErrorStatus.DUPLICATED_ID);
+        }
+
+        Certification certification = certificationRepository.findByLoginId(loginId)
+                .orElseThrow(() -> new CustomException(AuthErrorStatus.CERTIFICATION_NOT_EXIST));
+
+        if (!certification.isVerified()) {
+            throw new CustomException(AuthErrorStatus.CERTIFICATION_FAILED);
+        }
+
+        String encodedPassword = encoder.encode(signUpRequestDto.password());
+
+        User user = User.create(signUpRequestDto, encodedPassword);
+
+        userRepository.save(user);
+        certificationRepository.delete(certification);
     }
 }
