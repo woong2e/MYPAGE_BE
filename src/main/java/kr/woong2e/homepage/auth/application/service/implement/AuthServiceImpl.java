9e225a08a0a09dc688a6990d3f97f2972fa20d79
woong2e <tlswodnd12345@naver.com>
2025-06-23 23:56:12 +0900

    [feat]: 공통 검증 유틸 클래스(AssertUtil)를 추가한다. (#6)
    - 비즈니스 조건 검증을 위한 check 메서드 구현
    - Optional 검증 및 커스텀 예외 발생을 위한 orElseThrow 메서드 구현

--- src/main/java/kr/woong2e/homepage/auth/application/service/implement/AuthServiceImpl.java
+++ src/main/java/kr/woong2e/homepage/auth/application/service/implement/AuthServiceImpl.java
@@ -9,8 +9,8 @@ import kr.woong2e.homepage.auth.application.response.status.AuthErrorStatus;
 import kr.woong2e.homepage.auth.application.service.AuthService;
 import kr.woong2e.homepage.auth.domain.Certification;
 import kr.woong2e.homepage.auth.domain.CertificationRepository;
-import kr.woong2e.homepage.global.exception.CustomException;
 import kr.woong2e.homepage.global.provider.EmailProvider;
+import kr.woong2e.homepage.global.util.AssertUtil;
 import kr.woong2e.homepage.user.domain.User;
 import kr.woong2e.homepage.user.domain.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -38,9 +38,7 @@ public class AuthServiceImpl implements AuthService {
         String email = emailCertificationRequestDto.getEmail();
 
         boolean isExistsId = userRepository.existsByLoginId(loginId);
-        if (isExistsId) {
-            throw new CustomException(AuthErrorStatus.DUPLICATED_ID);
-        }
+        AssertUtil.check(isExistsId, AuthErrorStatus.DUPLICATED_ID);
 
         Certification certification = Certification.create(loginId, email);
         String certificationNumber = certification.getCertificationNumber();
@@ -55,8 +53,9 @@ public class AuthServiceImpl implements AuthService {
         String email = checkCertificationRequestDto.getEmail();
         String certificationNumber = checkCertificationRequestDto.getCertificationNumber();
 
-        Certification certification = certificationRepository.findByLoginId(loginId)
-                .orElseThrow(() -> new CustomException(AuthErrorStatus.CERTIFICATION_NOT_EXIST));
+        Certification certification = AssertUtil.orElseThrow(
+                certificationRepository.findByLoginId(loginId),
+                AuthErrorStatus.CERTIFICATION_NOT_EXIST);
 
         certification.verify(email, certificationNumber);
         certificationRepository.save(certification);
@@ -66,25 +65,20 @@ public class AuthServiceImpl implements AuthService {
     public void signUp(SignUpRequestDto signUpRequestDto) {
         String loginId = signUpRequestDto.loginId();
         boolean isExistId = userRepository.existsByLoginId(loginId);
-        if (isExistId) {
-            throw new CustomException(AuthErrorStatus.DUPLICATED_ID);
-        }
 
-        if (!signUpRequestDto.password().equals(signUpRequestDto.passwordCheck())) {
-            throw new CustomException(AuthErrorStatus.NOT_MATCHED_PASSWORD);
-        }
+        AssertUtil.check(!isExistId, AuthErrorStatus.DUPLICATED_ID);
+        AssertUtil.check(signUpRequestDto.password().equals(signUpRequestDto.passwordCheck()), AuthErrorStatus.NOT_MATCHED_PASSWORD);
 
-        Certification certification = certificationRepository.findByLoginId(loginId)
-                .orElseThrow(() -> new CustomException(AuthErrorStatus.CERTIFICATION_NOT_EXIST));
+        Certification certification = AssertUtil.orElseThrow(
+                certificationRepository.findByLoginId(loginId),
+                AuthErrorStatus.CERTIFICATION_NOT_EXIST
+        );
 
-        if (!certification.isVerified()) {
-            throw new CustomException(AuthErrorStatus.CERTIFICATION_FAILED);
-        }
+        AssertUtil.check(certification.isVerified(), AuthErrorStatus.CERTIFICATION_FAILED);
 
         String encodedPassword = encoder.encode(signUpRequestDto.password());
 
         User user = User.create(signUpRequestDto, encodedPassword);
-
         userRepository.save(user);
         certificationRepository.delete(certification);
     }
